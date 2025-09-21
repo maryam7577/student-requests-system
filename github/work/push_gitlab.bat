8475b129b21aea09c846dd8c7fdb90b430b473ed
Maryam Radman <maryam@example.com>
2025-09-21 15:36:38 +0300

    Add CI/CD workflow

---
+++ github/work/push_gitlab.bat
@@ -0,0 +1,11 @@
+
+Cd "C:\Users\Elite\Desktop\Student Requests System"
+git init
+git add .   
+
+git commit -m "Initial commit"
+git branch _M main
+git remote add github  https://github.com/maryam7577/student-requests-system.git
+
+git push-u origin main
+
